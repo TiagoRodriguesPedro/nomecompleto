9dd1d449b46a30b42eacd64d442ae8440239c97d
Tiago Rodrigues <isatiago098@gmail.com>
2021-11-05 21:18:39 -0300

    resoluçao exercicio nomecompleto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("ola " + nome);
+        
+            Console.Write(" por favor digite seu nome completo....");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("olá " + nomeCompleto);
+            Console.ReadKey();
+           Console.WriteLine("obrigado tenha uma boa noite");
+
+
+        }
+    }
+}
